5673b52e426e86a9fc153fed46989e27882c1453
Ängla Juliusson <angla02@icloud.com>
2025-09-19 10:11:36 +0200

    Get rout för work experiences

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get("/api", (req, res) => {
     res.json({message: "Welcome to my API"});
 });
 
+app.get("/api/work_experiences", (req, res) => {
+    res.json({message: "Get work experiences"});
+});
+
 app.listen(port, () => {
     console.log('Server is running on port: ' + port);
 });
