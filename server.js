e4810b9876128c6280a8086931c6fc681d3795d6
Ängla Juliusson <angla02@icloud.com>
2025-09-19 10:03:47 +0200

    Routes

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express =  require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Routes
+app.get("/api", (req, res) => {
+    res.json({message: "Welcome to my API"});
+});
+
 app.listen(port, () => {
     console.log('Server is running on port: ' + port);
 });
