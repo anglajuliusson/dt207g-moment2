60e745d338ede1fbb866ce8995eefeb514b5ec94
Ängla Juliusson <angla02@icloud.com>
2025-09-19 11:05:35 +0200

    Kunna ta emot JSON-data

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors()); // Inkludera cors i express-applikationen
+app.use(express.json()); // Sidan ska kunna ta emot JSON-data
 
 // Routes
 app.get("/api", (req, res) => {
